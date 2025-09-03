640114d92577fdd9eb5414ebe05e8b18cae35d08
Mohamed Al-Haffar <mallaser87@gmail.com>
2025-09-03 04:34:56 +0300

    ارجاع تعديلات رقم 3

---
+++ lib/constants/api.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const String baseUrl = "https://abshir-api.justfortesting.ovh/api";
+  static const String sendOtp = "$baseUrl/auth/send-otp"; // رجعناه
+  static const String verifyOtp = "$baseUrl/cwm"; // حسب لانا للتحقق
+}
