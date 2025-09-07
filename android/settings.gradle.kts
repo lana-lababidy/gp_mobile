a1ec9eae1f3e982d43b7714d85ddeac2b2dc77da
Mohamed Al-Haffar <mallaser87@gmail.com>
2025-09-07 03:31:53 +0300

    update

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -16,34 +16,32 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        // مستودع Flutter (لمنع البلغ-إن من إضافته لاحقاً على مستوى المشروع)
+        // مستودع Flutter
         maven(url = "https://storage.googleapis.com/download.flutter.io")
     }
 
-    // تعريف نسخ البلغ-إن على مستوى المشروع
+    // تعاريف نسخ البلغ-إن
     plugins {
         id("com.android.application") version "8.7.3"
-        id("org.jetbrains.kotlin.android") version "1.9.24"
+        id("org.jetbrains.kotlin.android") version "1.9.24"     // ثبّتنا كوتلن
         id("com.google.gms.google-services") version "4.4.2"
         id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     }
 }
 
-// حلّ التبعيات للمشاريع — اسمح بمستودعات المشروع لأن بلَغ-إن Flutter يضيف maven خاص
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS) // 👈 بدل FAIL_ON_PROJECT_REPOS
+    // اسمح بمستودعات المشروع (الـ Flutter plugin بيضيف maven خاص)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://storage.googleapis.com/download.flutter.io") // 👈 مستودع Flutter
+        maven(url = "https://storage.googleapis.com/download.flutter.io")
     }
 }
 
-// لا نطبّق أي بلَغ-إن هنا مباشرة؛ التطبيق يتم داخل :app
+// 👇 مهم: طبّق Flutter plugin loader (بدون apply false)
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0" apply false
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
 }
 
 rootProject.name = "android"
